e09e05415ace81791f1102bfeb24087e723a59eb
skpaik <sdiptapaik@gmail.com>
2023-12-22 02:58:35 +0600

    # Feature - Cart Item List all test cases added

--- components/CartForm/CartItemList.tsx
+++ components/CartForm/CartItemList.tsx
@@ -11,14 +11,14 @@ const CartItemList: React.FC<CartItemListProps> = ({cartItems}) => {
         <ul className="flex flex-col pt-4 space-y-2">
             {cartItems.map((item, index) => (
                 <li key={index} className="flex items-start justify-between">
-                    <h3>
+                    <h3 data-testid={"item-name-" + item.id}>
                         {item.name}
                         <span className="text-sm text-violet-400"> x </span>
                         {item.quantity}
                     </h3>
                     <div className="text-right">
-                        <span className="block">${(item.unit_price * item.quantity).toFixed(2)}</span>
-                        <span className="text-sm text-gray-400">à ${item.unit_price.toFixed(2)}</span>
+                        <span  data-testid={"item-total-price-" + item.id} className="block">$ {(item.unit_price * item.quantity).toFixed(2)}</span>
+                        <span  data-testid={"item-unit-price-" + item.id} className="text-sm text-gray-400">$ {item.unit_price.toFixed(2)}</span>
                     </div>
                 </li>
             ))}
